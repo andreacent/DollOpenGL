a25bdd7d254885ee03f810207cf47a6e33d0bf22
andreacent <andreacent8@gmail.com>
2016-04-27 23:18:15 -0430

    tipito listo con colores

--- main.cpp
+++ main.cpp
@@ -39,7 +39,6 @@ void ejesCoordenada(float w) {
 
                     glVertex2f(0.2,i);
                     glVertex2f(-0.2,i);
-
                 }
             }
         }
@@ -77,7 +76,7 @@ void drawCircle(float px, float py, float radio) {
 
 //  DIBUJA UN PUNTO (ARTICULACION)
 void drawPoint(float x, float y){
-    glColor3f(255,0,0);
+    glColor3f(1,0,0);
     glPointSize(8.0);
     glBegin(GL_POINTS);
         glVertex2f(x, y);
@@ -94,6 +93,47 @@ void drawLine(float x, float y){
     drawPoint(0.0,0.0); // el origen es la articulacion
 }
 
+// DIBUJA LA PIERNA
+void drawLeg(float x, float y){
+    glPushMatrix();
+        glTranslatef(x,0.0,0.0); //muevo el eje de la pierna izq
+        glColor3f(0,0,1);
+        drawLine(0.0, y);
+        //rodilla
+        glColor3f(0,1,0);
+        glPushMatrix();
+            glTranslatef(0.0,y,0.0);
+            drawLine(0.0, y);
+            //tobillo
+            glColor3f(1,1,0);
+            glPushMatrix();
+                glTranslatef(0.0,y,0.0);
+                drawLine(x,0.0);
+            glPopMatrix();
+        glPopMatrix();
+    glPopMatrix();
+}
+
+// DIBUJA EL BRAZO
+void drawArm(float x, float xt){
+    glPushMatrix();
+        glTranslatef(xt,3.0,0.0);
+        glColor3f(0,0,1);
+        drawLine(x,0.0);
+        //codo
+        glColor3f(0,1,0);
+        glPushMatrix();
+            glTranslatef(x,0.0,0.0);
+            drawLine(x,0.0);
+            //muneca
+            glColor3f(1,1,0);
+            glPushMatrix();
+                glTranslatef(x,0.0,0.0);
+                drawLine(0.0,1.0);
+            glPopMatrix();
+        glPopMatrix();
+    glPopMatrix();  
+}
 
 void render(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -104,7 +144,7 @@ void render(){
     int loopX, loopZ;
 
     /* Render Grid */
-    glPushMatrix();
+   /* glPushMatrix();
         glColor3f( 0.0f, 0.7f, 0.7f );
         glBegin( GL_LINES );
             zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
@@ -120,14 +160,14 @@ void render(){
                 glVertex3f(  xExtent, zLocal, 0.0f );
             }
         glEnd();
-    glPopMatrix();
+    glPopMatrix(); */
     /* END Render Grid */
 
     glTranslatef(0.0,-1.0,0.0); 
 
     // Parte inferior del cuerpo
     glBegin(GL_LINES);
-        glColor3f(1.0,1.0,0.0);
+        glColor3f(0,1,1);
         glVertex2f(0.0, 0.0);
         glVertex2f(0.0, 3.5);
         glVertex2f(-1.5, 0.0);
@@ -135,100 +175,42 @@ void render(){
     glEnd();
 
     // Pierna Izquierda
-    glPushMatrix();
-        glTranslatef(-1.5,0.0,0.0); //muevo el eje de la pierna izq
-        glColor3f(1.0,1.0,0.0);
-        drawLine(0.0, -4.0);
-        //rodilla
-        glPushMatrix();
-            glTranslatef(0.0,-4.0,0.0);
-            glColor3f(1.0,1.0,0.0);
-            drawLine(0.0, -4.0);
-            //tobillo
-            glPushMatrix();
-                glTranslatef(0.0,-4.0,0.0);
-                glColor3f(1.0,1.0,0.0);
-                drawLine(-1.5,0.0);
-            glPopMatrix();
-        glPopMatrix();
-    glPopMatrix();
+    drawLeg(-1.5, -4.0);
 
     // Pierna Derecho
-    glPushMatrix();
-        glTranslatef(1.5,0.0,0.0); //muevo el eje de la pierna der
-        glColor3f(1.0,1.0,0.0);
-        drawLine(0.0, -4.0);
-        //rodilla
-        glPushMatrix();
-            glTranslatef(0.0,-4.0,0.0);
-            glColor3f(1.0,1.0,0.0);
-            drawLine(0.0, -4.0);
-            //tobillo
-            glPushMatrix();
-                glTranslatef(0.0,-4.0,0.0);
-                glColor3f(1.0,1.0,0.0);
-                drawLine(1.5,0.0);
-            glPopMatrix();
-        glPopMatrix();
-    glPopMatrix();
+    drawLeg(1.5, -4.0);
 
     // Parte superior del cuerpo
     glPushMatrix();
         glTranslatef(0.0,3.5,0.0); //muevo el eje del dorso
-        //ejesCoordenada(1.0); //borrar
+
+        //dorso y hombros
         glBegin(GL_LINES);
-            glColor3f(1.0,1.0,0.0);
+            glColor3f(0,1,1);
             glVertex2f(0.0, 0.0);
             glVertex2f(0.0, 3.5);
+            glColor3f(1,0,1);
             glVertex2f(-2.0, 3.0);
             glVertex2f(2.0, 3.0);
         glEnd();
+        drawPoint(0.0,0.0);
 
         // Cabeza
         glPushMatrix();
             glTranslatef(0.0,3.5,0.0); //muevo el eje de la cabeza
-            glColor3f(1.0,1.0,0.0);
+            glColor3f(0,1,1);
             //ejesCoordenada(1.0); //borrar
             drawCircle(0.0,1.5,1.5);
+            drawPoint(0.0,0.0);
         glPopMatrix();
 
         // Brazo izquierdo
-        glPushMatrix();
-            glTranslatef(-2.0,3.0,0.0);
-            glColor3f(1.0,1.0,0.0);
-            drawLine(-2.5,0.0);
-            //codo
-            glPushMatrix();
-                glTranslatef(-2.5,0.0,0.0);
-                glColor3f(1.0,1.0,0.0);
-                drawLine(-2.5,0.0);
-                //muneca
-                glPushMatrix();
-                    glTranslatef(-2.5,0.0,0.0);
-                    glColor3f(1.0,1.0,0.0);
-                    drawLine(0.0,1.0);
-                glPopMatrix();
-            glPopMatrix();
-        glPopMatrix();  
+        drawArm(-2.5, -2.0);
 
         // Brazo derecho
-        glPushMatrix();
-            glTranslatef(2.0,3.0,0.0);
-            glColor3f(1.0,1.0,0.0);
-            drawLine(2.5,0.0);
-            //codo
-            glPushMatrix();
-                glTranslatef(2.5,0.0,0.0);
-                glColor3f(1.0,1.0,0.0);
-                drawLine(2.5,0.0);
-                //muneca
-                glPushMatrix();
-                    glTranslatef(2.5,0.0,0.0);
-                    glColor3f(1.0,1.0,0.0);
-                    drawLine(0.0,1.0);
-                glPopMatrix();
-            glPopMatrix();
-        glPopMatrix();  
+        drawArm(2.5, 2.0);
+
+    glPopMatrix();
 
     glutSwapBuffers();
 }
